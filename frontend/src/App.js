304a9424084c96a751d07842293c4993484fc9fb
sophiaungur <104379389+sophiaungur@users.noreply.github.com>
2023-02-04 15:47:18 -0500

    slaying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import WelcomeMessage from './components/welcome/welcome';
 import Landing from './components/landing/landing';
 import Login from './components/login/login';
+import Signup from './components/signup/signup';
 
 function App() {
   return (
     <div className="App">
       <section className='full-h'>
-        <Landing />
+        <Signup />
       </section>
     </div>
   );
