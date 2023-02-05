50667cf70867d247acd42b1cfbfb8716e134b356
Jordan <jomez68@gmail.com>
2023-02-04 21:30:05 -0500

    FUuuuuuu ckin balls

--- frontend/src/components/toggleButton/toggleButton.js
+++ frontend/src/components/toggleButton/toggleButton.js
@@ -1,11 +1,14 @@
 
 import { useState } from "react";
-export default function ToggleButton({ title, isActive }) {
+export default function ToggleButton({ title, isActive, icon}) {
     const [toggle, setToggle] = useState(isActive);
     return (
         <button className={toggle ? "btn tertiary extra-lite-text x-padding" : "btn extra-lite dark-text x-padding"} 
         onClick={() => setToggle(!toggle)}>
+            <div className="flex-center row gap">
+            {icon}
             {title}
+            </div>
         </button>
     );
 }
